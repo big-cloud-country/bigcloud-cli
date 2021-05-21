83fd1aa31f324de63f6874256b7cac1c18554db7
Rob Whelan <whelan@gmail.com>
2021-05-21 16:28:32 -0500

    cleanup file structure

--- robbie_2.js
+++
@@ -1,1 +0,0 @@
-var costData = [{"Group hierarchy":"My Estimate","Region":"US East (Ohio)","Description":"d","Service":"Amazon Athena","Upfront":0.0,"Monthly":74259.28,"First 12 months total":891111.36,"Currency":"USD","Configuration summary":"Total number of queries (5000 per day), Data amount scanned per query (100 GB)"}];
